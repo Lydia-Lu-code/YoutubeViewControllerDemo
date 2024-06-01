41a6107cb28b8611e5f3af99ea360e37963a6d83
Lydia <guipopo5@gmail.com>
2024-06-01 16:02:40 +0800

    Feat： 新增原生綁定方法

--- YoutubeViewController/Utilities/Observable.swift
+++ YoutubeViewController/Utilities/Observable.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+// 原生觀察者綁定
+
+
+class Observable <T> {
+    
+    typealias Helper =  (T) -> Void
+    var helper:Helper?
+    
+    var value:T {
+        didSet {
+            helper?(value)
+        }
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    // 更改畫面值時,呼叫的方法
+    func bind (completion: @escaping Helper) {
+        self.helper = completion
+    }
+}
